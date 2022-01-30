bf1df60435a256967e558e79ee14fd3fd9c6afa2
APRXMT <69224310+APRXMT@users.noreply.github.com>
2022-01-29 20:19:38 -0600

    Main form now navigable. Scenes now linked to appropriate controllers.

--- src/controller/addPartController.java
+++ src/controller/addPartController.java
@@ -3,12 +3,16 @@ package controller;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.RadioButton;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -57,7 +61,12 @@ public class addPartController implements Initializable {
     }
 
     @FXML
-    void onActionGotoMainForm(ActionEvent event) {
+    void onActionGotoMainForm(ActionEvent event) throws IOException {
+/*
+        stage = (Stage)((Button)event.getSource()).getScene().getWindow();
+        scene = FXMLLoader.load(getClass().getResource("/view/addPart.fxml"));
+        stage.setScene(new Scene(scene));
+        stage.show();*/
 
     }
 
--- src/controller/mainFormController.java
+++ src/controller/mainFormController.java
@@ -2,16 +2,24 @@ package controller;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class mainFormController implements Initializable {
+
+    Stage stage;
+    Parent scene;
 
     @FXML
     private TableColumn<?, ?> partPriceCol;
@@ -72,43 +80,65 @@ public class mainFormController implements Initializable {
 
 
     @FXML
-    void onActionAddPart(ActionEvent event) {
+    void onActionAddPart(ActionEvent event) throws IOException {
 
+        stage = (Stage)((Button)event.getSource()).getScene().getWindow();
+        scene = FXMLLoader.load(getClass().getResource("/view/addPart.fxml"));
+        stage.setScene(new Scene(scene));
+        stage.show();
     }
 
     @FXML
-    void onActionModifyPart(ActionEvent event) {
+    void onActionModifyPart(ActionEvent event) throws IOException {
+
+        stage = (Stage)((Button)event.getSource()).getScene().getWindow();
+        scene = FXMLLoader.load(getClass().getResource("/view/modifyPart.fxml"));
+        stage.setScene(new Scene(scene));
+        stage.show();
 
     }
 
     @FXML
     void onActionDeletePart(ActionEvent event) {
 
+        System.out.println("Delete Part Request");
     }
 
     @FXML
-    void onActionAddProduct(ActionEvent event) {
+    void onActionAddProduct(ActionEvent event) throws IOException {
+
+        stage = (Stage)((Button)event.getSource()).getScene().getWindow();
+        scene = FXMLLoader.load(getClass().getResource("/view/addProduct.fxml"));
+        stage.setScene(new Scene(scene));
+        stage.show();
 
     }
 
     @FXML
-    void onActionModifyProduct(ActionEvent event) {
+    void onActionModifyProduct(ActionEvent event) throws IOException {
+
+        stage = (Stage)((Button)event.getSource()).getScene().getWindow();
+        scene = FXMLLoader.load(getClass().getResource("/view/modifyProduct.fxml"));
+        stage.setScene(new Scene(scene));
+        stage.show();
 
     }
 
     @FXML
     void onActionDeleteProduct(ActionEvent event) {
 
+        System.out.println("Delete Product Request");
     }
 
     @FXML
     void onActionExitProgram(ActionEvent event) {
-        System.out.println("Exit button Pressed");
+        System.exit(0);
     }
 
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
+
         System.out.println("I am initialized!");
     }
 }
