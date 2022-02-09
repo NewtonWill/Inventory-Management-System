a0826c2e914dfd70e169791dd05e3d2f72287e4b
APRXMT <69224310+APRXMT@users.noreply.github.com>
2022-02-08 18:15:38 -0600

    Add part save initial implementation

--- src/controller/addPartController.java
+++ src/controller/addPartController.java
@@ -12,6 +12,9 @@ import javafx.scene.control.Label;
 import javafx.scene.control.RadioButton;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
+import model.InHouse;
+import model.Inventory;
+import model.Outsourced;
 
 import java.io.IOException;
 import java.net.URL;
@@ -27,7 +30,7 @@ public class addPartController implements Initializable {
     private TextField partPriceTxt;
 
     @FXML
-    private Label altIdLabel;
+    private Label altIdLabel; //fixme figure out how to change label on radio switch
 
     @FXML
     private TextField partMaxTxt;
@@ -60,8 +63,50 @@ public class addPartController implements Initializable {
     private Button saveBtn;
 
     @FXML
-    void onActionSavePart(ActionEvent event) {
+    void onActionSavePart(ActionEvent event) throws IOException {
 
+        //int id =        Integer.parseInt(partIdTxt.getText());
+        int id = 5; //fixme temp until autogen is figured out
+
+        String name =   partNameTxt.getText();
+        int inv =       Integer.parseInt(partInvTxt.getText());
+        double price =  Double.parseDouble(partPriceTxt.getText());
+        int max =       Integer.parseInt(partMaxTxt.getText());
+        int min =       Integer.parseInt(partMinTxt.getText());
+        int machineId = Integer.parseInt(altIdTxt.getText());
+        String companyName = altIdTxt.getText();
+
+
+        if (inhouseRadio.isSelected()) {
+            Inventory.addPart(new InHouse(id, name, price, inv, min, max,
+                    machineId));
+            System.out.println("In-House part added");
+
+            stage = (Stage) ((Button) event.getSource()).getScene().getWindow();
+            scene = FXMLLoader.load(getClass().getResource("/view/mainForm.fxml"));
+            stage.setScene(new Scene(scene));
+            stage.show();
+
+            return;
+        }
+
+        if (outsourceRadio.isSelected()) {
+            Inventory.addPart(new Outsourced(id, name, price, inv, min, max,
+                    companyName)); //fixme figure out why this isn't accepting non ints
+            System.out.println("Outsourced part added");
+
+            stage = (Stage) ((Button) event.getSource()).getScene().getWindow();
+            scene = FXMLLoader.load(getClass().getResource("/view/mainForm.fxml"));
+            stage.setScene(new Scene(scene));
+            stage.show();
+
+            return;
+        }
+
+
+        else {
+            System.out.println("Error with input");
+        }
     }
 
     @FXML
